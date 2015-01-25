42749a723203573b5548a13d6eebfc8931660f16
IevaLek <wushia@gmail.com>
2015-01-25 00:10:15 +0000

    Fixed db migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,6 @@ ActiveRecord::Schema.define(version: 20150125000040) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
