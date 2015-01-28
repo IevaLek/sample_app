c04967d56f7117a948d6d96204613e80bbfe550f
IevaLek <wushia@gmail.com>
2015-01-28 19:28:38 +0000

    Changes to user test model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-  	@user = User.new(name: "Example User", email: "user@example.com")
+  	@user = User.new(name: "Example User", email: "user@test.com", password: "xxx", password_confirmation: "xxx")
   end
 
   test "should be valid" do 
@@ -31,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do
-  	valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+  	valid_addresses = %w[user@test.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
   	valid_addresses.each do |valid_address|
   		@user.email = valid_address
   		assert @user.valid?, "#{valid_address.inspect} should be valid"
